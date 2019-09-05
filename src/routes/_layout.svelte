ab09e81c419d2e112cb2a3c705e5556b10ecdc04
Andrea Salamone <andrea.sala96@gmail.com>
2019-09-05 22:00:40 +0200

    feat: add footer

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -3,9 +3,21 @@
 </script>
 
 <style>
-  @import "../style.scss";
+  @import "../style/style.scss";
 </style>
 
-<main class="container-fluid">
+<main class="container min-vh-100">
   <slot></slot>
 </main>
+
+<footer class="container py-5">
+  <div class="row">
+    <div class="col-md order-md-last pt-4 text-md-right">
+      <a href="https://svelte.dev/" target="_blank" rel="noreferrer">svelte</a>
+      <a href="https://github.com/lukehaas/getlorem" target="_blank" rel="noreferrer">getlorem</a>
+    </div>
+    <div class="col-md pt-4">
+      &copy; <a href="https://andsala.dev/" target="_blank">Andrea Salamone</a>
+    </div>
+  </div>
+</footer>
